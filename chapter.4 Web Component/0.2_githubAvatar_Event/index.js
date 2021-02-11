70e9ade328558d94da219f932c89759cb05ac994
choiseunghyeon <ket8780@gmail.com>
2021-02-11 19:25:31 +0900

    chapter 4. Web Component Event

---
+++ chapter.4 Web Component/0.2_githubAvatar_Event/index.js
@@ -0,0 +1,12 @@
+import GithubAvatar, { EVENTS } from "./components/GithubAvatar.js";
+
+window.customElements.define("github-avatar", GithubAvatar);
+
+document.querySelectorAll("github-avatar").forEach(avatar => {
+  avatar.addEventListener(EVENTS.AVATAR_LOAD_COMPLETE, e => {
+    console.log("Avatar Loaded", e.detail.avatar);
+  });
+  avatar.addEventListener(EVENTS.AVATAR_LOAD_ERROR, e => {
+    console.log("Avatar Loading Error", e.detail.error);
+  });
+});
