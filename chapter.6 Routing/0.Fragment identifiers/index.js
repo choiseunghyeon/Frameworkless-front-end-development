edfdf8ab8fb4b9b659a0b52dbfc9b37829c023a4
choiseunghyeon <ket8780@gmail.com>
2021-02-12 17:30:53 +0900

    chapter 6. Routing / Fragment Identifiers

---
+++ chapter.6 Routing/0.Fragment identifiers/index.js
@@ -0,0 +1,10 @@
+import createRouter from "./rotuer.js";
+import createPages from "./pages.js";
+
+const container = document.querySelector("main");
+
+const pages = createPages(container);
+
+const router = createRouter();
+
+router.addRoute("#/", pages.home).addRoute("#/list", pages.list).setNotFound(pages.notFound).start();
